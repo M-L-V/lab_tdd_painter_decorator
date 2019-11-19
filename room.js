2d20a68778a0564c23df9576c1aa3522bb686336
Morgaine <morgainedv@gmail.com>
2019-11-19 19:40:47 +0000

    first room functions

---
+++ room.js
@@ -0,0 +1,9 @@
+const Room = function(areaInMeters, paintedArea) {
+  this.areaInMeters = areaInMeters;
+  this.paintedArea = 0;
+}
+
+
+
+
+module.exports = Room;
--- specs/room_spec.js
+++ specs/room_spec.js
@@ -1,2 +1,25 @@
 const assert = require('assert');
 const Room = require('../room.js');
+
+
+describe('Room', function(){
+  let room;
+  beforeEach(function() {
+    room = new Room( 100, 0) //arrange
+  });
+
+  it('should have an area in sq meters', function(){
+    const actual = room.areaInMeters;        //act
+    assert.strictEqual(actual, 100);   //assert
+  });
+
+  it('should be unpainted', function() {
+    const actual = room.paintedArea;              //act
+    assert.deepStrictEqual(actual, 0);    //assert //use an x before it to prevent test from running
+  });
+
+
+
+
+
+});
